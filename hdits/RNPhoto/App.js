ce426d72229b36b113becc87ca54518089d61f9b
박상훈 <psanghun@bagsanghun-ui-iMac.local>
2018-12-05 00:41:31 +0900

    view

--- hdits/RNPhoto/App.js
+++ hdits/RNPhoto/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import Search from './component/Search';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Search style={styles.search} />
-      </View>
+      <ScrollView style={{ flex: 1, marginTop: 30 }}>
+        <View style={styles.container}>
+          <Search style={styles.search} />
+          <Search style={styles.search} />
+          <Search style={styles.search} />
+          <Search style={styles.search} />
+          <Search style={styles.search} />
+          <Search style={styles.search} />
+          <Search style={styles.search} />
+          <Search style={styles.search} />
+        </View>
+      </ScrollView>
     );
   }
 }
